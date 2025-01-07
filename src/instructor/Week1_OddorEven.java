8ef9198f2cb92d2f7e5ffa3962f6872619ec095d
Owen <Owen.constantine@cydeo.com>
2025-01-07 23:00:47 +0300

    Java Task 1 question

---
+++ src/instructor/Week1_OddorEven.java
@@ -0,0 +1,17 @@
+package instructor;
+
+
+/*
+Numbers  ---Odd & Even
+write a method which can identifies if a given number is even or odd.
+
+
+        EX:
+             identify(5) -> "Odd"
+             identify(6) -> "Even"
+*/
+
+public class Week1_OddorEven {
+
+
+ }
